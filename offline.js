39118f778f5e219690cd4eb5fe7709a03a36dfbd
MPGames <mpgamesdeveloper@gmail.com>
2019-07-30 15:20:08 -0400

    small update

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1564499258,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/destinoplayer-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/tiledbackground.png",
+		"images/setaplayer-sheet0.png",
+		"images/luzplayer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
